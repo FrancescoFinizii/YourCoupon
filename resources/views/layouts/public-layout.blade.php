44c973a6deb1820565d505456671d0aeeda92151
Francesco Finizii <s1099052@studenti.univpm.it>
2023-05-30 14:00:03 +0200

    stable version level1

--- resources/views/layouts/public-layout.blade.php
+++ resources/views/layouts/public-layout.blade.php
@@ -7,7 +7,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
-    <script src="{{asset("js/javascript.js")}}"></script>
+    <script src="{{asset("js/francesco.js")}}"></script>
     <link rel="stylesheet" href="{{asset ("css/francesco/elements.css") }}">
     <link rel="stylesheet" href="{{asset ("css/francesco/style.css") }}">
 </head>
